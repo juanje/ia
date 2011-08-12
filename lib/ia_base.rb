e77b24e75f8fc9491ddb1228b2f0bd3acd08f3b2
Jose Alberto <ja@josealberto.org>
2011-08-12 11:06:56 +0200

    mv ia_version -> ia_base. Añado librería para drupal

---
+++ lib/ia_base.rb
@@ -0,0 +1,4 @@
+module Ia
+  AUTHOR  = 'JoseAlberto'
+  VERSION = '0.0.1'
+end
---
+++ lib/ia_drupal.rb
@@ -0,0 +1,26 @@
+module Ia::Drupal
+
+  # Gestion de archivos para drupal
+  module Files
+    # Busca archivos potencialmente inseguros y los borra si existen
+    # pretend (Boolean): si es verdadero solo imprime las modificaciones
+    def drupal_rm_files(pretend)
+      txt_files = %w(CHANGELOG.txt CONTRIBUTORS.es.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.txt LICENSE.es.txt LICENSE.txt MAINTAINERS.txt README.es.txt STATUS.es.txt UPGRADE.txt)
+      php_files = %w(install.php phpinfo.php)
+      files = txt_files + php_files
+      files.each do |f|
+        if pretend
+          pinfo("rm #{f}")  if FileTest.exist?(f)
+        else
+          system("rm #{f}") if FileTest.exist?(f)
+        end
+      end
+    end
+
+    def drupal_secure_files(pretend)
+      files = %w(upgrade.php cron.php)
+    end
+  end
+
+end
+
