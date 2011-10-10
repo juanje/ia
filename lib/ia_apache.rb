5675737511693d1fc3c7de93ec1a9e16179d7d48
Jose Alberto <ja@josealberto.org>
2011-10-10 13:51:10 +0200

    mejorado cración de virtualhosts en queca

--- lib/ia_apache.rb
+++ lib/ia_apache.rb
@@ -4,26 +4,51 @@ module Ia::Apache
   def apache_gen_config_file(name,path)
 
     config = ""
-    %w(dev test).each do |i|
-      unless i == "test" && queca?
-        config << %{<VirtualHost *:80>
-  ServerAdmin webmaster@localhost
-  ServerName #{name}-#{i}.emergya-gpi.es
-  ServerAlias *.#{name}-#{i}.emergya-gpi.es
-  DocumentRoot /var/www/projects/#{name}/trunk
-  <Directory /var/www/projects/#{name}/trunk>
-    Options FollowSymLinks
-    AllowOverride All
-    Order deny,allow
-    Allow from all
-  </Directory>
+    if queca?
+      domain = "queca.lan"
 
-  ErrorLog /var/log/apache2/projects/#{name}-#{i}-error.log
-  LogLevel warn
-  CustomLog /var/log/apache2/projects/#{name}-#{i}-access.log combined
-  ServerSignature On
-</VirtualHost>
-}
+      config << %{<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    ServerName #{name}.#{domain}
+    ServerAlias *.#{name}.#{domain}
+    DocumentRoot /var/www/projects/#{name}/trunk
+    
+    <Directory /var/www/projects/#{name}/trunk>
+      Options FollowSymLinks
+      AllowOverride All
+      Order deny,allow
+      Allow from all
+    </Directory>
+
+    ErrorLog /var/log/apache2/projects/#{name}-error.log
+    LogLevel warn
+    CustomLog /var/log/apache2/projects/#{name}-access.log combined
+    ServerSignature On
+  </VirtualHost>
+  }
+    else
+      domain = "emergya-gpi.es"
+
+      %w(dev test).each do |i|
+        config << %{<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    ServerName #{name}-#{i}.#{domain}
+    ServerAlias *.#{name}-#{i}.#{domain}
+    DocumentRoot /var/www/projects/#{name}/trunk
+    
+    <Directory /var/www/projects/#{name}/trunk>
+      Options FollowSymLinks
+      AllowOverride All
+      Order deny,allow
+      Allow from all
+    </Directory>
+
+    ErrorLog /var/log/apache2/projects/#{name}-#{i}-error.log
+    LogLevel warn
+    CustomLog /var/log/apache2/projects/#{name}-#{i}-access.log combined
+    ServerSignature On
+  </VirtualHost>
+  }
       end
     end
 
